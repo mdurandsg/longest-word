c5266de8b9adda8265c35fe253c8a244a8782cc8
Michel Durand <michel.durand@socgen.com>
2018-11-27 15:32:27 +0100

    Game development with TDD

---
+++ game.py
@@ -0,0 +1,26 @@
+"""Module docstring"""
+import random
+import string
+
+class Game(): # pylint: disable=too-few-public-methods
+    """docstring for Game"""
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+        print(self.grid)
+
+    def is_valid(self, word):
+        """docstring of is_valid method"""
+        if not word:
+            return False
+        letters = self.grid[:]
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
+
+if __name__ == '__main__':
+    Game()
